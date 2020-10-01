85b1f1fe0b9f8c742cd24beba90403a47ea316c7
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-09-30 18:49:29 -0700

    Update decode_variations.py

--- code/decode_variations.py
+++ code/decode_variations.py
@@ -15,3 +15,48 @@ explanation: There are 3 messages that encode to '1262': 'AZB', 'ABFB', and 'LFB
 
 """
 
+
+# Recursive
+# O(n), O(n)
+def decodeVariations(S):
+  def dfs(i):
+    if i == len(S):
+      return 1
+    if i in memo:
+      return memo[i]
+    
+    count = 0
+    if 1 <= int(S[i]) <= 9:
+      count += dfs(i+1)
+    if 10 <= int(S[i:i+2]) <= 26:
+      count += dfs(i+2)
+    memo[i] = count
+    return count
+    
+  memo = {}
+  return dfs(0)
+
+
+# Iterative (DP)
+# O(n), O(1)
+def decodeVariations(S):
+  if not S or S[0] == "0":
+    return 0
+  
+  prev = 1
+  curr = 1
+  
+  for i in range(1, len(S)):
+    temp = curr
+    if S[i] == "0":
+      if S[i-1] == "1" or S[i-1] == "2":
+        curr = prev
+      else:
+        return 0
+    elif 10 <= int(S[i-1:i+1]) <= 26:
+      curr += prev
+    prev = temp
+    
+  return curr
+
+        
