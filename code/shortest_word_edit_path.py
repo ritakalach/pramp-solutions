93df1e969fd57e4efd68154aac8663de7357db56
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-05 15:35:17 -0700

    Create shortest_word_edit_path.py

---
+++ code/shortest_word_edit_path.py
@@ -0,0 +1,20 @@
+"""
+Shortest Word Edit Path
+
+Given two words source and target, and a list of words words, find the length of the shortest series of edits that transforms source to target.
+
+Each edit must change exactly one letter at a time, and each intermediate word (and the final target word) must exist in words.
+
+If the task is impossible, return -1.
+
+
+source = "bit", target = "dog"
+words = ["but", "put", "big", "pot", "pog", "dog", "lot"]
+
+output: 5
+explanation: bit -> but -> put -> pot -> pog -> dog has 5 transitions.
+source = "no", target = "go"
+words = ["to"]
+
+output: -1
+"""
