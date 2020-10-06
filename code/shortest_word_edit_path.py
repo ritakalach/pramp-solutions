160d68fb2549e9b5602ff4a3d0a30f31970bb640
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-05 17:58:45 -0700

    Update shortest_word_edit_path.py

--- code/shortest_word_edit_path.py
+++ code/shortest_word_edit_path.py
@@ -18,3 +18,25 @@ words = ["to"]
 
 output: -1
 """
+
+import string     
+from collections import deque 
+
+def shortestWordEditPath(source, target, words):
+  alphabet = string.ascii_lowercase
+  words_set = set(words)
+  queue = deque([(source, 0)])
+  seen = set()
+  
+  while queue:
+    curr_word, depth = queue.popleft()
+    if curr_word == target:
+      return depth
+    for i in range(len(curr_word)):
+      for char in alphabet:
+        potential_next_word = curr_word[:i] + char + curr_word[i+1:]
+        if potential_next_word in words_set and potential_next_word not in seen:
+          queue.append((potential_next_word, depth+1))
+          seen.add(potential_next_word)
+          
+  return -1
