46597f3e7c72c70bf351bcdfbed46c10e63ab6b9
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-09 16:14:41 -0700

    Update getting_diff_num.py

--- code/getting_diff_num.py
+++ code/getting_diff_num.py
@@ -3,15 +3,12 @@ Getting a Different Number
 
 Given an array arr of unique nonnegative integers, implement a function getDifferentNumber that finds the smallest nonnegative integer that is NOT in the array.
 
-
 input:  arr = [0, 1, 2, 3]
 output: 4 
 
 input:  arr = [0, 2, 3]
 output: 1
-
 """
-
 
 # O(n)
 # O(n)
@@ -24,7 +21,6 @@ def get_different_number(arr):
       return i
     
   return n
-
 
 # O(n)
 # O(1)
