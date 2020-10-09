9ea189ec4489aafe6a244f8b4191f7e708060585
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-09 16:16:06 -0700

    Update flatten_dictionary.py

--- code/flatten_dictionary.py
+++ code/flatten_dictionary.py
@@ -30,16 +30,14 @@ output: {
 """        
 
 def flatten_dictionary(nested_dict):
-  flat_dict = {} 
+  flat_dict = {}
   flat_dict_recurse(nested_dict, flat_dict)
   return flat_dict
 
-def flat_dict_recurse(nested_dict, flat_dict, key_path=""):
+def flat_dict_recurse(nested_dict, flat_dict, key_path = ""):
   for key, value in nested_dict.items():
     include_dot = 1 if key_path and key else 0
-    # Case 1: not a nested dict, so we can add item to final output 
     if not isinstance(value, dict):
-        flat_dict[key_path + ("." * include_dot) + key] = value
-    # Case 2: nested dict, we recurse
+      flat_dict[key_path + "." * include_dot + key] = value
     else:
-        flat_dict_recurse(value, flat_dict, key_path + ("." * include_dot) + key)  
+      flat_dict_recurse(value, flat_dict, key_path + "." * include_dot + key)
