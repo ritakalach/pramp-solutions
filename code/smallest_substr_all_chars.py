643698a6181bf72058374b238a05c1d6390d4340
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-19 06:40:25 -0700

    Update smallest_substr_all_chars.py

--- code/smallest_substr_all_chars.py
+++ code/smallest_substr_all_chars.py
@@ -1,5 +1,5 @@
 """
-Smallest Substring of All Characters
+Smallest Substring of All Characters (Minimum Window Substring)
 
 Given an array of unique characters arr and a string str, Implement a function getShortestUniqueSubstring that finds the smallest substring of str 
 containing all the characters in arr. Return "" (empty string) if such a substring doesn’t exist.
