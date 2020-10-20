ea7a8d9333f05a901b0a3d9f72cb0e1561dfbe19
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-20 16:37:18 -0700

    Update word_count_engine.py

--- code/word_count_engine.py
+++ code/word_count_engine.py
@@ -1,3 +1,27 @@
+"""
+Word Count Engine
+
+Implement a document scanning function wordCountEngine, which receives a string document and returns a list of all unique words in it 
+and their number of occurrences, sorted by the number of occurrences in a descending order. If two or more words have the same count, 
+they should be sorted according to their order in the original sentence. Assume that all letters are in english alphabet. 
+You function should be case-insensitive, so for instance, the words “Perfect” and “perfect” should be considered the same word.
+
+The engine should strip out punctuation (even in the middle of a word) and use whitespaces to separate words.
+
+
+input:  document = "Practice makes perfect. you'll only
+                    get Perfect by practice. just practice!"
+
+output: [ ["practice", "3"], ["perfect", "2"],
+          ["makes", "1"], ["youll", "1"], ["only", "1"], 
+          ["get", "1"], ["by", "1"], ["just", "1"] ]
+Important: please convert the occurrence integers in the output list to strings (e.g. "3" instead of 3).
+
+"""
+
+# Let N be the number of words in document and M the number of unique words in it (M ≤ N)
+# O(N) time
+# O(M) space
 from collections import OrderedDict
 
 def word_count_engine(document):
