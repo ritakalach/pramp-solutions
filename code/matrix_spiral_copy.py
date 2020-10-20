1f9d551ff165cfc035cc740d4846b519104f100f
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-19 21:55:34 -0700

    Create matrix_spiral_copy.py

---
+++ code/matrix_spiral_copy.py
@@ -0,0 +1,15 @@
+"""
+Matrix Spiral Copy
+
+Given a 2D array (matrix) inputMatrix of integers, create a function spiralCopy that copies inputMatrix’s values into a 1D array in a spiral order, clockwise. 
+Your function then should return that array. Analyze the time and space complexities of your solution.
+
+
+input:  inputMatrix  = [ [1,    2,   3,  4,    5],
+                         [6,    7,   8,  9,   10],
+                         [11,  12,  13,  14,  15],
+                         [16,  17,  18,  19,  20] ]
+
+output: [1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12]
+
+"""
