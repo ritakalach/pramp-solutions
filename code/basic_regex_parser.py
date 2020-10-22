a55b7b4f75ff8a7be75b847b8e456848bd27470e
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-22 08:32:13 -0700

    Update basic_regex_parser.py

--- code/basic_regex_parser.py
+++ code/basic_regex_parser.py
@@ -24,3 +24,27 @@ input:  text = "acd", pattern = "ab*c."
 output: true
 
 """
+
+def is_match(text, pattern):
+  m, n = len(text), len(pattern)
+  if not m and not n:
+    return True
+  if not pattern: # e.g. text = "a" pattern = ""
+    return False
+  
+  dp = [[False for j in range(n + 1)] for i in range(m + 1)]
+  dp[0][0] = True
+  for j in range(1, n + 1):
+    if pattern[j - 1] == "*":
+      dp[0][j] = dp[0][j-1] or (dp[0][j - 2] if j > 1 else False)
+  
+  for i in range(1, m + 1):
+    for j in range(1, n + 1):
+      if text[i - 1] == pattern[j - 1] or pattern[j - 1] == ".":
+        dp[i][j] = dp[i - 1][j - 1]
+      elif pattern[j - 1] == "*":
+          dp[i][j] = dp[i][j - 1] or (dp[i][j - 2] if j > 1 else False)
+          if j > 1 and (text[i - 1] == pattern[j - 2] or pattern[j - 2] == "."):
+            dp[i][j] = dp[i][j] or dp[i - 1][j] 
+
+  return dp[-1][-1]
