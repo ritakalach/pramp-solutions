3c069da5dab68a0b65756fad4842dc00ef2c1a79
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-10-29 20:15:42 -0700

    Update deletion_distance.py

--- code/deletion_distance.py
+++ code/deletion_distance.py
@@ -25,9 +25,8 @@ output: 0
 # O(mn) time
 # O(mn) space
 def deletion_distance(str1, str2):
-  deletions = 0
   if str1 == str2:
-    return deletions
+    return 0
   
   m, n = len(str1), len(str2)
   dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
@@ -49,3 +48,61 @@ def deletion_distance(str1, str2):
         dp[i][j] = dp[i - 1][j - 1]
         
   return dp[-1][-1]
+
+
+# Same as previous
+def deletion_distance(str1, str2):
+  if str1 == str2:
+    return 0
+  
+  m, n = len(str1), len(str2)
+  dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+
+  for i in range(m + 1):
+    for j in range(n + 1):
+      if i == 0:
+        dp[i][j] = j
+      elif j == 0:
+        dp[i][j] = i
+      elif str1[i - 1] != str2[j - 1]:
+        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1
+      else:
+        dp[i][j] = dp[i - 1][j - 1]
+        
+  return dp[-1][-1]
+
+
+# O(mn) time
+# O(min(n,m)) space
+"""
+Pseudocode:
+
+function deletionDistance(str1, str2):
+    # make sure the length of str2 isn't
+    # longer than the length of str1
+    if (str1.length < str2.length)
+        tmpStr = str1
+        str1 = str2
+        str2 = tmpStr
+
+    str1Len = str1.length
+    str2Len = str2.length
+    prevMemo = new Array(str2Len  + 1)
+    currMemo = new Array(str2Len  + 1)
+
+    for i from 0 to str1Len:
+        for j from 0 to str2Len:
+            if (i == 0):
+                currMemo[j] = j
+            else if (j == 0):
+                currMemo[j] = i
+            else if (str1[i-1] == str2[j-1]):
+                currMemo[j] = prevMemo[j-1]
+            else:
+                currMemo[j] = 1 + min(prevMemo[j], currMemo[j-1])
+
+        prevMemo = currMemo
+        currMemo = new Array(str2Len + 1);  
+                                           
+    return prevMemo[str2Len]
+"""
